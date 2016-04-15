0fcdf8c4ccebeeae0eb9e36c0e17eec04a7c019b
Kevin Reiss <kevin.reiss@gmail.com>
2016-04-15 10:28:43 -0500

    Update comments, move method.

--- app/models/requests/requestable.rb
+++ app/models/requests/requestable.rb
@@ -19,5 +19,16 @@ module Requests
     def aeon?
     end
 
+    def available?
+    end
+
+    # This should a property of requestable. The Router can invoke this test when it looks
+    # at item status and finds something unavailable. Need to confirm with Peter Bae if the only monographs rule holds
+    # true for borrow direct. Currenly if a Record is not a serial/multivolume no Borrow Direct
+    def borrow_direct_eligible?
+      if @requestable.item[:status] == 'check for not available' and self.doc[:format] == 'Book'
+        return true
+      end
+    end
   end
 end

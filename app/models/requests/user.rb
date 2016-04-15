63bbfe0f1cc41add32dc3cb53140ae023909f72e
Kevin Reiss <kevin.reiss@gmail.com>
2016-04-15 17:05:45 -0500

    WIP specs for request class.

--- app/models/requests/user.rb
+++ app/models/requests/user.rb
@@ -1,4 +1,10 @@
 module Requests
   class User
+    attr_accessor :username
+    attr_accessor :email
+    attr_accessor :uid
+    attr_accessor :provider
+    attr_accessor :password
+    attr_accessor :guest
   end
 end

31f525e3cd12b859d201c6506655880ef5a690b2
Kevin Reiss <kevin.reiss@gmail.com>
2016-04-21 18:19:52 -0500

    WIP Request class with test scenarios for common bib, holdings, item combos.

--- app/models/requests/requestable.rb
+++ app/models/requests/requestable.rb
@@ -1,12 +1,18 @@
 module Requests
   class Requestable
 
+    attr_reader :bib
+    attr_reader :holding
+    attr_reader :item
+
     def initialize(params)
       @bib = params[:bib] # has of bib values
       @holding = params[:holding] || nil # hash of holding values
-      @location = params[:location] || nil # hash of location matrix data
+      #@location = params[:location] || nil # hash of location matrix data
       @item = params[:item] || nil # hash of item values
     end
+
+    def 
 
     def location_code
       @holding[:location_code]
@@ -22,6 +28,9 @@ module Requests
     def available?
     end
 
+    def item?
+      @item
+    end
     # This should a property of requestable. The Router can invoke this test when it looks
     # at item status and finds something unavailable. Need to confirm with Peter Bae if the only monographs rule holds
     # true for borrow direct. Currenly if a Record is not a serial/multivolume no Borrow Direct

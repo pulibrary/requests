4d1fc107f344af323a060b24d50917b1d8de5095
Kevin Reiss <kevin.reiss@gmail.com>
2016-06-07 12:56:11 -0500

    spacing tweaks.

--- app/helpers/requests/request_helper.rb
+++ app/helpers/requests/request_helper.rb
@@ -7,7 +7,7 @@ module Requests
     def current_user_status current_user
       ## Expect that the host app can provide you a devise current_user object
       if current_user.provider == 'cas'
-        content_tag(:dl, class: "flash_messages-user") do
+        content_tag(:div, class: "flash_messages-user") do
           content_tag(:div, I18n.t('requests.account.pul_auth', current_user_name: current_user.uid), class: "flash-alert")
         end
       elsif current_user.guest == true

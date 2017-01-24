b515925b855c7f1dd175bbd7d372eac64cbd755a
Shaun Ellis <shaun@sdellis.com>
2017-01-24 12:13:51 -0500

    handles records with no item attached to holding
    fixes broken tests
    
    removes redundancy
    
    removes unnecessary row in request form

--- app/models/requests/submission.rb
+++ app/models/requests/submission.rb
@@ -14,16 +14,17 @@ module Requests
         if selected['selected'] == 'true'
           if selected.key? 'user_supplied_enum'
             if selected['user_supplied_enum'].empty?
-              record.errors[:items].merge!({ "user_supplied_#{selected['mfhd']}" => { 'text' => 'Please Fill in additional volume information', 'type' => 'options' } })
+              #record.errors[:items] << { selected['mfhd'] => { 'text' => 'Please Fill in additional volume information.', 'type' => 'options' } }
+              record.errors[:items] << { "user_supplied_#{selected['mfhd']}" => { 'text' => 'Please Fill in additional volume information', 'type' => 'options' } }
             end
           end
-        
+
           if selected["type"].blank?
             record.errors[:items] << { selected['mfhd'] => { 'text' => 'Please choose a Request Method for your selected item.', 'type' => 'pickup' } }
           end
 
           if mail_services.include?(selected["type"]) || selected["type"] == 'recall'
-            if selected['pickup'].empty? && selected['item_id'].empty?
+            if selected['pickup'].empty? && selected['item_id'].blank?
               record.errors[:items] << { selected['mfhd'] => { 'text' => 'Please select a pickup location.', 'type' => 'pickup' } }
             elsif selected['pickup'].empty?
               record.errors[:items] << { selected['item_id'] => { 'text' => 'Please select a pickup location.', 'type' => 'pickup' } }

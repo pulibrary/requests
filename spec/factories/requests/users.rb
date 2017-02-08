4d0446d03713b89e30fec77f1502eca256c093ab
Kevin Reiss <kevin.reiss@gmail.com>
2017-02-08 17:12:49 -0600

    Add barcode user factory and access patron messages.

--- spec/factories/requests/users.rb
+++ spec/factories/requests/users.rb
@@ -4,22 +4,20 @@ FactoryGirl.define do
   factory :user, class: "User" do
     sequence(:username) { |n| "username#{srand}" }
     sequence(:email) { |n| "email-#{srand}@princeton.edu" }
+    provider 'cas'
     password 'foobarfoo'
     uid do |user|
       user.username
     end
 
-    factory :valid_princeton_patron do
-      provider 'cas'
-    end
-
-    # for patrons without a net ID
-    factory :valid_access_patron do
-      provider 'voyager'
-    end
+    # factory :valid_princeton_patron do
+    #   provider 'cas'
+    # end
 
     factory :valid_barcode_patron do
       provider 'barcode'
+      sequence(:uid) { srand.to_s[2..15] }
+      username 'Student'
     end
 
     factory :unauthenticated_patron do

30707559361e42cbc7aab6268b55403be99aa9c4
Shaun Ellis <shaun@sdellis.com>
2017-02-15 17:01:24 -0500

    All three auth options support

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Requests::Engine.routes.draw do
   get "/", to: 'request#index'
-  get '/pageable', to: 'request#pageable'
-  get ':system_id', to: 'request#generate' #, constraints: { system_id: /(\d|^dsp).+/ }
   get '/borrow_direct', to: 'request#bd'
-  post '/barcode_auth', to: 'request#barcode_auth'
   post '/recall_pickups', to: 'request#recall_pickups'
   post '/submit', to: 'request#submit'
+  get '/pageable', to: 'request#pageable'
+  get ':system_id', to: 'request#generate' #, constraints: { system_id: /(\d|^dsp).+/ }
+  post ':system_id', to: 'request#generate'
   root to: "request#index"
 end

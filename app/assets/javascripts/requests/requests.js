579ca32daf17b15a9e890bd5f340d2ce9179b5c3
Shaun Ellis <shaun@sdellis.com>
2017-02-23 17:17:41 -0500

    Fixes issues #88 and #90

--- app/assets/javascripts/requests/requests.js
+++ app/assets/javascripts/requests/requests.js
@@ -2,6 +2,33 @@
 // All this logic will automatically be available in application.js.
 $(document).ready(function() {
 
+    function isEmail(email) {
+      var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      return regex.test(email);
+    }
+
+    $( "form#logins" ).submit(function( event ) {
+      if ( !isEmail($( "#request_email" ).val()) ) {
+        event.preventDefault();
+        $( "#request_email" ).css("background-color","#f2dede");
+        $( "span.error-email").css("color", "red");
+        $( "span.error-email").text("Please supply a valid email address.");
+      } else {
+        $( "#request_email" ).css("background-color","#ffffff");
+        $( "span.error-email").text("");
+      }
+      if ($.trim($('#request_user_name').val()) == "")
+      {
+        event.preventDefault();
+        $( "#request_user_name" ).css("background-color","#f2dede");
+        $( "span.error-user_name").css("color", "red");
+        $( "span.error-user_name").text("Please supply your full name.");
+      } else {
+        $( "#request_user_name" ).css("background-color","#ffffff");
+        $( "span.error-user_name").text("");
+      }
+    });
+
     $( "#no_netid").click(function( event ) {
       event.preventDefault();
       $( "#no_netid").hide();

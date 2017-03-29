43ba3062793724b9caaacd46544c88622c2a4a0c
Shaun Ellis <shaun@sdellis.com>
2017-03-29 13:08:01 -0400

    Adds unauthenticated user integration tests

--- spec/features/requests/request_spec.rb
+++ spec/features/requests/request_spec.rb
@@ -6,26 +6,115 @@ describe 'request', vcr: { cassette_name: 'request_features', record: :new_episo
   let(:thesis_id) { 'dsp01rr1720547' }
   let(:valid_patron_response) { fixture('/bibdata_patron_response.json') }
   let(:valid_barcode_patron_response) { fixture('/bibdata_patron_response_guest.json') }
-  
-  describe 'When visiting without a system ID' do
-    it "Displays a Not Valid System ID error" do
-      visit '/requests'
-      expect(page).to have_content "Please Supply a valid Library ID to Request"
+
+  context 'all patrons' do
+
+    describe 'When visiting without a system ID' do
+      it "Displays a Not Valid System ID error" do
+        visit '/requests'
+        expect(page).to have_content "Please Supply a valid Library ID to Request"
+      end
+    end
+
+    describe 'When unauthenticated patron visits a request item', js: true do
+      it "displays three authentication options" do
+        visit '/requests/9944355'
+        expect(page).to have_content(I18n.t('requests.account.netid_login_msg'))
+        expect(page).to have_content(I18n.t('requests.account.barcode_login_msg'))
+        expect(page).to have_content(I18n.t('requests.account.other_user_login_msg'))
+      end
     end
   end
 
-  # # when current_user is available test these
-  # describe 'When visitng with a system id' do
-  #   it 'from Voyager' do
-  #     visit '/requests/9493318'
-  #     expect(page).to have_content voyager_id
-  #   end
+  context 'unauthenticated patron' do
+    describe 'When visiting a request item without logging in', js: true do
 
-  #   it 'from a Theses' do
-  #     visit "/requests/#{thesis_id}"
-  #     expect(page).to have_content thesis_id
-  #   end
-  # end
+        it 'allows guest patrons to identify themselves and view the form' do
+          visit '/requests/9944355'
+          click_link(I18n.t('requests.account.other_user_login_msg'))
+          fill_in 'request_email', :with => 'name@email.com'
+          fill_in 'request_user_name', :with => 'foobar'
+          click_button I18n.t('requests.account.other_user_login_btn')
+          wait_for_ajax
+          expect(page).to have_content 'ReCAP Oversize DT549 .E274q'
+        end
+
+        it 'allows guest patrons to request a physical recap item' do
+          visit '/requests/9944355'
+          click_link(I18n.t('requests.account.other_user_login_msg'))
+          fill_in 'request_email', :with => 'name@email.com'
+          fill_in 'request_user_name', :with => 'foobar'
+          click_button I18n.t('requests.account.other_user_login_btn')
+          expect(page).to have_no_content 'Electronic Delivery'
+          select('Firestone Library', :from => 'requestable__pickup')
+          click_button 'Request this Item'
+          wait_for_ajax
+          expect(page).to have_content 'Request submitted'
+        end
+
+        it 'prohibits guest patrons from requesting In-Process items' do
+          visit '/requests/9646099'
+          click_link(I18n.t('requests.account.other_user_login_msg'))
+          fill_in 'request_email', :with => 'name@email.com'
+          fill_in 'request_user_name', :with => 'foobar'
+          click_button I18n.t('requests.account.other_user_login_btn')
+          expect(page).to have_content 'In Process'
+          expect(page).to have_content 'Item is not requestable.'
+        end
+
+        it 'prohibits guest patrons from requesting On-Order items' do
+          visit '/requests/10081566'
+          click_link(I18n.t('requests.account.other_user_login_msg'))
+          fill_in 'request_email', :with => 'name@email.com'
+          fill_in 'request_user_name', :with => 'foobar'
+          click_button I18n.t('requests.account.other_user_login_btn')
+          expect(page).to have_content 'Item is not requestable.'
+        end
+
+        it 'allows guest patrons to access Online items' do
+          visit '/requests/9994692'
+          click_link(I18n.t('requests.account.other_user_login_msg'))
+          fill_in 'request_email', :with => 'name@email.com'
+          fill_in 'request_user_name', :with => 'foobar'
+          click_button I18n.t('requests.account.other_user_login_btn')
+          expect(page).to have_content 'www.jstor.org'
+        end
+
+        it 'allows guest patrons to request Aeon items' do
+          visit '/requests/561774?mfhd=612742'
+          click_link(I18n.t('requests.account.other_user_login_msg'))
+          fill_in 'request_email', :with => 'name@email.com'
+          fill_in 'request_user_name', :with => 'foobar'
+          click_button I18n.t('requests.account.other_user_login_btn')
+          click_link('Request to View in Reading Room')
+          expect(page).to have_content 'Special Collections Research Account'
+        end
+
+        it 'prohibits guest patrons from using Borrow Direct, ILL, and Recall on Missing items' do
+          visit '/requests/1788796?mfhd=2053005'
+          click_link(I18n.t('requests.account.other_user_login_msg'))
+          fill_in 'request_email', :with => 'name@email.com'
+          fill_in 'request_user_name', :with => 'foobar'
+          click_button I18n.t('requests.account.other_user_login_btn')
+          expect(page).to have_content 'Item is not requestable.'
+        end
+
+        it 'allows guests to request from Annex, but not from Firestone in mixed holding' do
+          visit '/requests/2286894'
+          click_link(I18n.t('requests.account.other_user_login_msg'))
+          fill_in 'request_email', :with => 'name@email.com'
+          fill_in 'request_user_name', :with => 'foobar'
+          click_button I18n.t('requests.account.other_user_login_btn')
+          expect(page).to have_field 'requestable__selected', disabled: false
+          expect(page).to have_field 'requestable_selected_7484608', disabled: true
+          check('requestable__selected', exact: true)
+          select('Firestone Library', :from => 'requestable__pickup')
+          click_button 'Request Selected Items'
+          wait_for_ajax
+          expect(page).to have_content 'Request submitted'
+        end
+    end
+  end
 
   context 'a princeton net ID user' do
     let(:user) { FactoryGirl.create(:user) }

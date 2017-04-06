6eb800715f6bbd0c897036e5fc9d60e32306dea8
Kevin Reiss <kevin.reiss@gmail.com>
2017-04-06 12:43:55 -0500

    Initial demo for #125.
    Display use restrictions and enumerations. Add hidden fields for SCSB. Advances #125.
    
    SCSB key comes from environment.
    
    SCSB param map.
    
    indifferent access strikes again.

--- app/helpers/requests/application_helper.rb
+++ app/helpers/requests/application_helper.rb
@@ -131,7 +131,7 @@ module Requests
       if !item[:enum_display].nil? && !item[:copy_number].nil?
         display += " "
       end
-      unless item[:copy_number].nil? || item[:copy_number] == 0 || item[:copy_number] == 1
+      unless item[:copy_number].nil? || item[:copy_number] == 0 || item[:copy_number] == 1 || item[:copy_number] == '1'
         display += "Copy #{item[:copy_number]}"
       end
       display
@@ -215,7 +215,11 @@ module Requests
       unless requestable.holding.first[1]["call_number"].nil?
         hidden += hidden_field_tag "requestable[][call_number]", "", value: "#{requestable.holding.first[1]['call_number']}", id: "requestable_call_number_#{requestable.item['id']}"
       end
-      hidden += hidden_field_tag "requestable[][location_code]", "", value: "#{requestable.item["location"]}", id: "requestable_location_#{requestable.item['id']}"
+      if requestable.item["location"].nil?
+        hidden += hidden_field_tag "requestable[][location_code]", "", value: "#{requestable.location['code']}", id: "requestable_location_#{requestable.item['id']}"
+      else 
+        hidden += hidden_field_tag "requestable[][location_code]", "", value: "#{requestable.item["location"]}", id: "requestable_location_#{requestable.item['id']}"
+      end
       hidden += hidden_field_tag "requestable[][item_id]", "", value: "#{requestable.item["id"]}", id: "requestable_item_id_#{requestable.item['id']}"
       unless requestable.item["barcode"].nil?
         hidden += hidden_field_tag "requestable[][barcode]", "", value: "#{requestable.item["barcode"]}", id: "requestable_barcode_#{requestable.item['id']}"
@@ -223,8 +227,17 @@ module Requests
       unless requestable.item["enum"].nil?
         hidden += hidden_field_tag "requestable[][enum]", "", value: "#{requestable.item["enum"]}", id: "requestable_enum_#{requestable.item['id']}"
       end
+      ### This should cleaned up 
+      unless requestable.item["enumeration"].nil?
+        hidden += hidden_field_tag "requestable[][enum]", "", value: "#{requestable.item["enumeration"]}", id: "requestable_enum_#{requestable.item['id']}"
+      end
       hidden += hidden_field_tag "requestable[][copy_number]", "", value: "#{requestable.item["copy_number"]}", id: "requestable_copy_number_#{requestable.item['id']}"
       hidden += hidden_field_tag "requestable[][status]", "", value: "#{requestable.item["status"]}", id: "requestable_status_#{requestable.item['id']}"
+      if requestable.scsb?
+        hidden += hidden_field_tag "requestable[][collection_group]", "", value: "#{requestable.item["collection_group"]}", id: "requestable_collection_group_#{requestable.item['id']}"
+        hidden += hidden_field_tag "requestable[][designation]", "", value: "#{requestable.item["designation"]}", id: "requestable_designation_#{requestable.item['id']}"
+        hidden += hidden_field_tag "requestable[][use_statement]", "", value: "#{requestable.item["use_statement"]}", id: "requestable_use_statement_#{requestable.item['id']}"
+      end
       hidden
     end
 

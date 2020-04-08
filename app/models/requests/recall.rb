bed8a45f69626096b69f7f9581b7b72e83f90e31
Carolyn Cole <cac9@princeton.edu>
2020-04-08 11:27:18 -0400

    fixing more MethodLength errors

--- app/models/requests/recall.rb
+++ app/models/requests/recall.rb
@@ -18,26 +18,7 @@ module Requests
       items = @submission.filter_items_by_service(@service_type)
       scsb_params = {}
       items.each do |item|
-        # location = get_location(item['location_code'])
-        if scsb_locations.include? item['location_code'] # || (location[:library][:code] == 'recap')
-          params = scsb_param_mapping(@submission.bib, @submission.user, item)
-          if scsb_params.empty?
-            scsb_params = params
-          else
-            scsb_params[:itemBarcodes].push(item['barcode'])
-          end
-        else
-          params = param_mapping(@submission.bib, @submission.user, item)
-          payload = request_payload(item)
-          r = put_response(params, payload)
-          xml_response = Nokogiri::XML(r.body)
-          if xml_response.xpath("//reply-text").text == 'ok'
-            @sent << { bibid: params[:Bbid], item: params[:item], user_name: @submission.user[:user_name], patron: params[:patron] }
-          else
-            error_message = "Failed request: " + xml_response.xpath("//note").text
-            @errors << { bibid: params[:recordID], item: params[:itemID], user_name: @submission.user[:user_name], patron: params[:patron], error: error_message }
-          end
-        end
+        scsb_params = handle_item(item: item, scsb_params: scsb_params)
       end
 
       return false if scsb_params.empty?
@@ -61,5 +42,35 @@ module Requests
     end
 
     attr_reader :errors
+
+    private
+
+      def handle_item(item:, scsb_params:)
+        # location = get_location(item['location_code'])
+        if scsb_locations.include? item['location_code'] # || (location[:library][:code] == 'recap')
+          params = scsb_param_mapping(@submission.bib, @submission.user, item)
+          if scsb_params.empty?
+            scsb_params = params
+          else
+            scsb_params[:itemBarcodes].push(item['barcode'])
+          end
+        else
+          handle_non_scsb_recap_item(item: item)
+        end
+        scsb_params
+      end
+
+      def handle_non_scsb_recap_item(item:)
+        params = param_mapping(@submission.bib, @submission.user, item)
+        payload = request_payload(item)
+        r = put_response(params, payload)
+        xml_response = Nokogiri::XML(r.body)
+        if xml_response.xpath("//reply-text").text == 'ok'
+          @sent << { bibid: params[:Bbid], item: params[:item], user_name: @submission.user[:user_name], patron: params[:patron] }
+        else
+          error_message = "Failed request: " + xml_response.xpath("//note").text
+          @errors << { bibid: params[:recordID], item: params[:itemID], user_name: @submission.user[:user_name], patron: params[:patron], error: error_message }
+        end
+      end
   end
 end

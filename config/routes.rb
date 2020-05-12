35655e46aaa971369b8ab26557c721442abf4f75
christinach <actspatial@gmail.com>
2020-05-12 17:25:03 -0400

    correct typo

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Requests::Engine.routes.draw do
   post '/submit', to: 'request#submit'
   # no longer in use
   # get '/pageable', to: 'request#pageable'
-  get ':system_id', to: 'request#generate', constraints: { system_id: /(\d+|dsp\w+|SCSB-\d+|coins-\d+)/i }
-  post ':system_id', to: 'request#generate', constraints: { system_id: /(\d+|dsp\w+|SCSB-\d+|coins-\d+)/i }
+  get ':system_id', to: 'request#generate', constraints: { system_id: /(\d+|dsp\w+|SCSB-\d+|coin-\d+)/i }
+  post ':system_id', to: 'request#generate', constraints: { system_id: /(\d+|dsp\w+|SCSB-\d+|coin-\d+)/i }
   root to: "request#index"
 end

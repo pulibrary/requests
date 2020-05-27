30ec52a500f7a0051cbc6eea26e352e6e9e2990c
Carolyn Cole <cac9@princeton.edu>
2020-05-27 14:15:10 -0400

    Update the form to allow for edd submission
    Also reactivated the request feature test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -217,7 +217,7 @@ end
 
 def wait_for_ajax
   counter = 0
-  while page.execute_script('return $.active').to_i > 0
+  while page.execute_script('return document.active').to_i > 0
     counter += 1
     sleep(0.1)
     raise 'AJAX request took longer than 60 seconds.' if counter >= 60
@@ -226,7 +226,7 @@ end
 
 def wait_for_bd
   counter = 0
-  while page.execute_script('return $.active').to_i > 0
+  while page.execute_script('return document.active').to_i > 0
     counter += 1
     sleep(0.1)
     raise 'Borrow Direct request took longer than 60 seconds.' if counter >= 60

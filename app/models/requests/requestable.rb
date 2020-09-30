03cb12e14430ec9127d554fb935e89df28e8074d
Carolyn Cole <cac9@princeton.edu>
2020-09-30 15:36:00 -0400

    Adding check for training for fill in pick-up and pick-up fixes #836

--- app/models/requests/requestable.rb
+++ app/models/requests/requestable.rb
@@ -14,7 +14,7 @@ module Requests
     delegate :pageable_loc?, to: :@pageable
     delegate :map_url, to: :@mappable
     delegate :illiad_request_url, :illiad_request_parameters, to: :@illiad
-    delegate :campus_authorized, to: :@patron
+    delegate :campus_authorized, :covid_trained?, to: :@patron
 
     include Requests::Aeon
 
@@ -50,12 +50,12 @@ module Requests
     end
 
     def pick_up?
-      return false if user_barcode.blank? || etas? || !campus_authorized
-      item_data? && (on_shelf? || recap? || annexa?) && circulates? && !in_library_use_only? && !scsb_in_library_use? && !request?
+      return false if user_barcode.blank? || etas? || !covid_trained?
+      item_data? && (on_shelf? || recap? || annexa?) && circulates? && !in_library_use_only? && !scsb_in_library_use? && !request_status?
     end
 
     def fill_in_pickup?
-      return false if user_barcode.blank? || !campus_authorized
+      return false if user_barcode.blank? || !covid_trained?
       !item_data? || pick_up?
     end
 

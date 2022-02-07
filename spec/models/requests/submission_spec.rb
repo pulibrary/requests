2d3d5b1ca97fc1aecc2b589c9aa04c9824c27d3d
Xander Gardner <66753486+XanderGardner@users.noreply.github.com>
2022-02-07 15:04:30 -0500

    Renamed cgc to cgd (#1067)

--- spec/models/requests/submission_spec.rb
+++ spec/models/requests/submission_spec.rb
@@ -1087,7 +1087,7 @@ describe Requests::Submission do
           "enum_display" => "",
           "copy_number" => "1",
           "status" => "Available",
-          "cgc" => "",
+          "cgd" => "",
           "cc" => "",
           "use_statement" => "",
           "type" => "recap",
